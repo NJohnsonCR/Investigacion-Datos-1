2579e165718a2b566c63b03025cc3e6b6a252171
NJohnsonCR <60128330+NJohnsonCR@users.noreply.github.com>
2020-07-24 11:50:18 -0600

    Created Hello World from Azure

---
+++ Azure/AzureApp/AzureHelloWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureHelloWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
